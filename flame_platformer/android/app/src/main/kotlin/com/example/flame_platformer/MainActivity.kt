daaba0b3b644ff45d5876974d830e452e4d4456e
Tan Nguyen <93578088+nnminhtan@users.noreply.github.com>
2024-09-08 17:48:32 +0700

    adding the basic temp part 1

---
+++ flame_platformer/android/app/src/main/kotlin/com/example/flame_platformer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flame_platformer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flame_platformer/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame_platformer/flame_platformer.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
+  flameplatformer game = flameplatformer();
+  runApp(GameWidget(game: kDebugMode ? flameplatformer() : game),
+  
+  );
+}
