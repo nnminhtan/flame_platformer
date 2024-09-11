e83aae09c699f001c37cfc13023477d692128872
Tan Nguyen <93578088+nnminhtan@users.noreply.github.com>
2024-09-11 13:51:12 +0700

    first level is on but not finished

--- flame_platformer/lib/flame_platformer.dart
+++ flame_platformer/lib/flame_platformer.dart
@@ -1,3 +1,21 @@
-import 'package:flame/game.dart';
+import 'dart:async';
 
-class flameplatformer extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame_platformer/levels/level.dart';
+
+class flameplatformer extends FlameGame {
+
+  late final CameraComponent cam;
+  final world = Level();
+
+  
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(world: world, width: 1920, height: 1024);
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
---
+++ flame_platformer/lib/levels/level.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+class Level extends World{
+    late TiledComponent level;
+
+  @override
+  FutureOr<void> onLoad() async {
+
+    level = await TiledComponent.load('forestmap.tmx', Vector2.all(16));
+
+    add(level);
+
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
